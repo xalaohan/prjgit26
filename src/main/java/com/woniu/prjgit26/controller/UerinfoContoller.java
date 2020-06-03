4988a74d5e20f37b3cadc7efc77f374e1f9a9d2a
25559750@qq.com <bny760926>
2020-06-03 14:04:51 +0800

    UserinfoController:update:save1

--- src/main/java/com/woniu/prjgit26/controller/UerinfoContoller.java
+++ src/main/java/com/woniu/prjgit26/controller/UerinfoContoller.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UerinfoContoller {
     @GetMapping("/say")
     public String say(){
+        System.out.println(1);
         return "hello";
     }
 
